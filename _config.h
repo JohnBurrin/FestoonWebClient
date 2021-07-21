da4a9b565caa9d1fcb12ba61e2a6607e0e4085e1
John Burrin <john@jointine.co.uk>
2021-07-21 08:03:28 +0100

    config changes

--- _config.h
+++ _config.h
@@ -4,9 +4,9 @@
 const char *ssid = "BTHub5-PGFX_Private";
 const char *password = "c@Z5wvM!Ut";
 
-String sensorLocation = "Festoon Leg 2 Top";
-String sensorId = "S4";
-const char* host = "S4-festoon-sensor";
+String sensorLocation = "Roaming";
+String sensorId = "R1";
+const char* host = "R1-festoon-sensor";
 const char* update_path = "/firmware";
 const char* update_username = "admin";
 const char* update_password = "admin";
@@ -21,6 +21,6 @@ String apiKeyValue = "tPmAT5Ab3j7F9";
 String sensorName = "SI1702";
 
 // remove this define to not compile BMP280 sensor
-//#define _COMPILE_BMP_280
+#define _COMPILE_BMP_280
 
 #endif
